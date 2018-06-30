3b85c854982feccfdc133dbccaa2edde75e1975a
Jiazhuo <jiazhuo2713@gmail.com>
2018-06-30 13:32:46 -0400

    annotation face

--- service/main.go
+++ service/main.go
@@ -20,7 +20,8 @@ import (
 	"reflect"
   "github.com/pborman/uuid"
 
-  "cloud.google.com/go/bigtable"
+  "path/filepath"
+  //"cloud.google.com/go/bigtable"
 
 
 
@@ -35,22 +36,42 @@ type Location struct {
 type Post struct {
 	User string `json:"user"`
 	Message string `json:"message"`
-	Location Location `json:"location"` 
-  Url    string `json:"url"`
+	Location Location `json:"location"`
+        Url    string `json:"url"`
+        Type     string   `json:"type"`
+        Face     float64  `json:"face"`
+
 }
 const(
 	INDEX = "around"
 	TYPE = "post"
 	DISTANCE = "200km"
- 
-	ES_URL = "http://35.188.14.221:9200"
-  BUCKET_NAME = "post-images-207823"
-  PROJECT_ID = "around-207823"
-  BT_INSTANCE = "around-post"
+
+	ES_URL = "http://35.239.234.114:9200"
+        BUCKET_NAME = "post-images-207823"
+        PROJECT_ID = "around-207823"
+        BT_INSTANCE = "around-post"
+        API_PREFIX      = "/api/v1"
 
 )
 
 var mySigningKey = []byte("secret")
+
+
+var (
+        mediaTypes = map[string]string{
+                ".jpeg": "image",
+                ".jpg":  "image",
+                ".gif":  "image",
+                ".png":  "image",
+                ".mov":  "video",
+                ".mp4":  "video",
+                ".avi":  "video",
+                ".flv":  "video",
+                ".wmv":  "video",
+        }
+)
+
 
 func main() {// Create a client
   client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
@@ -95,12 +116,18 @@ func main() {// Create a client
          SigningMethod: jwt.SigningMethodHS256,
   })
 
-  r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
-  r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
-  r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
-  r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+        r.Handle(API_PREFIX+"/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost)))
+        r.Handle(API_PREFIX+"/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch)))
+        r.Handle(API_PREFIX+"/cluster", jwtMiddleware.Handler(http.HandlerFunc(handlerCluster)))
 
-  http.Handle("/", r)
+        r.Handle(API_PREFIX+"/login", http.HandlerFunc(loginHandler))
+        r.Handle(API_PREFIX+"/signup", http.HandlerFunc(signupHandler))
+
+        // Backend endpoints.
+        http.Handle(API_PREFIX+"/", r)
+        // Frontend endpoints.
+        http.Handle("/", http.FileServer(http.Dir("build")))
+
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -158,7 +185,30 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
              return
       }
 
-      // Update the media link after saving to GCS.
+        im, header, _ := r.FormFile("image")
+        defer im.Close()
+        suffix := filepath.Ext(header.Filename)
+
+        // Client needs to know the media type so as to render it.
+        if t, ok := mediaTypes[suffix]; ok {
+                p.Type = t
+        } else {
+                p.Type = "unknown"
+        }
+        // ML Engine only supports jpeg.
+        if suffix == ".jpeg" {
+                if score, err := annotate(im); err != nil {
+                        http.Error(w, "Failed to annotate the image", http.StatusInternalServerError)
+                        fmt.Printf("Failed to annotate the image %v\n", err)
+                        return
+                } else {
+                        p.Face = score
+                }
+        }
+
+
+
+        // Update the media link after saving to GCS.
       p.Url = attrs.MediaLink
 
       //part 1
@@ -173,6 +223,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
       // Save to BigTable.
       //saveToBigTable(p, id)
 
+      /*
       // you must update project name here
       bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
       if err != nil {
@@ -196,6 +247,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
              return
       }
       fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+      *///
 
 }
 
@@ -222,7 +274,7 @@ func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*stor
              return nil, nil, err
       }
 
-      //set control authority      
+      //set control authority
       if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
              return nil, nil, err
       }
@@ -265,10 +317,10 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
       	fmt.Println("Received one request for search")
       	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
       	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-      	// range is optional 
-      	ran := DISTANCE 
-      	if val := r.URL.Query().Get("range"); val != "" { 
-        	 ran = val + "km" 
+      	// range is optional
+      	ran := DISTANCE
+      	if val := r.URL.Query().Get("range"); val != "" {
+        	 ran = val + "km"
       	}
 
       	fmt.Println("range is ", ran)
@@ -325,7 +377,7 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
         w.Write(js)
 
 
-} 
+}
 
 
 // checkUser checks whether user is valid
@@ -398,9 +450,6 @@ func addUser(user User) bool {
   return true
 }
 
-// If signup is successful, a new session is created
-
-
 // If signup is successful, a new session is created.
 func signupHandler(w http.ResponseWriter, r *http.Request) {
       fmt.Println("Received one signup request")
@@ -464,6 +513,69 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
       w.Header().Set("Access-Control-Allow-Origin", "*")
 }
 
+
+func handlerCluster(w http.ResponseWriter, r *http.Request) {
+        fmt.Println("Received one request for clustering")
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Content-Type", "application/json")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+        if r.Method != "GET" {
+                return
+        }
+
+        term := r.URL.Query().Get("term")
+
+        // Create a client
+        client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+        if err != nil {
+                http.Error(w, "ES is not setup", http.StatusInternalServerError)
+                fmt.Printf("ES is not setup %v\n", err)
+                return
+        }
+
+        // Range query.
+        // For details, https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-range-query.html
+        q := elastic.NewRangeQuery(term).Gte(0.9)
+
+        searchResult, err := client.Search().
+                Index(INDEX).
+                Query(q).
+                Pretty(true).
+                Do()
+        if err != nil {
+                // Handle error
+                m := fmt.Sprintf("Failed to query ES %v", err)
+                fmt.Println(m)
+                http.Error(w, m, http.StatusInternalServerError)
+        }
+
+        // searchResult is of type SearchResult and returns hits, suggestions,
+        // and all kinds of other information from Elasticsearch.
+        fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+        // TotalHits is another convenience function that works even when something goes wrong.
+        fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+        // Each is a convenience function that iterates over hits in a search result.
+        // It makes sure you don't need to check for nil values in the response.
+        // However, it ignores errors in serialization.
+        var typ Post
+        var ps []Post
+        for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+                p := item.(Post)
+                ps = append(ps, p)
+
+        }
+        js, err := json.Marshal(ps)
+        if err != nil {
+                m := fmt.Sprintf("Failed to parse post object %v", err)
+                fmt.Println(m)
+                http.Error(w, m, http.StatusInternalServerError)
+                return
+        }
+
+        w.Write(js)
+}
 
 
 
